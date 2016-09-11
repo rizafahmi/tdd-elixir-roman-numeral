5b8d207b3979970752b3fc1de7beb9ef12b5b423
Riza Fahmi <rizafahmi@gmail.com>
2016-09-11 13:33:39 +0700

    Writing our first implementation. Green

--- lib/roman_numeral.ex
+++ lib/roman_numeral.ex
@@ -1,2 +1,14 @@
 defmodule RomanNumeral do
+  @moduledoc """
+  Method for creating Roman numerals from integers
+  """
+
+  @doc """
+  Create a roman numeral representation of a string
+  """
+
+  @spec create(integer) :: String.t
+  def create(0) do
+    "nvlla"
+  end
 end
